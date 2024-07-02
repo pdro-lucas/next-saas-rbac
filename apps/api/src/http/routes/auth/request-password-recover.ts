f34e981f4aa81eb6d4e4fa915df985cd8620ff99
pdro-lucas <pdrolucas.dev@gmail.com>
2024-07-01 21:36:14 -0300

    feat: Add password recovery functionality
    This commit adds the necessary code to handle password recovery for users. It includes the creation of a new route for requesting password recovery and resetting the password using a recovery code.

---
+++ apps/api/src/http/routes/auth/request-password-recover.ts
@@ -0,0 +1,44 @@
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function requestPasswordRecover(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/password/recover',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Get authenticated user profile',
+        body: z.object({
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (!userFromEmail) {
+        return reply.status(201)
+      }
+
+      const { id: code } = await prisma.token.create({
+        data: {
+          type: 'PASSWORD_RECOVER',
+          userId: userFromEmail.id,
+        },
+      })
+
+      // Send email with password recover link
+      console.log('Recover password link:', code)
+    },
+  )
+}
