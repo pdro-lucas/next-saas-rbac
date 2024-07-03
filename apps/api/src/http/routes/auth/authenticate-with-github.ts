041a31cca488d63c3aeab3c4dcebea0a60a5da79
pdro-lucas <pdrolucas.dev@gmail.com>
2024-07-02 21:36:54 -0300

    feat: Add GitHub authentication functionality
    This commit adds the necessary code to authenticate users with GitHub. It includes the creation of a new route for authenticating with GitHub and obtaining an access token. The user's GitHub account information is retrieved and used to create or update the user's profile in the database.

---
+++ apps/api/src/http/routes/auth/authenticate-with-github.ts
@@ -0,0 +1,140 @@
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function authenticateWithGithub(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/sessions/github',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Authenticate with github and password',
+        body: z.object({
+          code: z.string(),
+        }),
+        response: {
+          201: z.object({
+            token: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { code } = request.body
+
+      const githubOAuthURL = new URL(
+        'https://github.com/login/oauth/access_token',
+      )
+
+      githubOAuthURL.searchParams.set('client_id', 'Ov23liHHqctvW8zlvIZQ')
+      githubOAuthURL.searchParams.set(
+        'client_secret',
+        'ef464b4a6d6c57127860564e950852d92eb60b27',
+      )
+      githubOAuthURL.searchParams.set(
+        'redirect_uri',
+        'http://localhost:3000/api/auth/callback',
+      )
+      githubOAuthURL.searchParams.set('code', code)
+
+      const githubAccessTokenResponse = await fetch(githubOAuthURL, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+
+      const githubAccessTokenData = await githubAccessTokenResponse.json()
+
+      const { access_token: githubAccessToken } = z
+        .object({
+          access_token: z.string(),
+          token_type: z.literal('bearer'),
+          scope: z.string(),
+        })
+        .parse(githubAccessTokenData)
+
+      const githubUserResponse = await fetch('https://api.github.com/user', {
+        headers: {
+          Authorization: `Bearer ${githubAccessToken}`,
+        },
+      })
+
+      const githubUserData = await githubUserResponse.json()
+
+      const {
+        id: githubId,
+        name,
+        email,
+        avatar_url: avatarUrl,
+      } = z
+        .object({
+          id: z.number().int().transform(String),
+          name: z.string().nullable(),
+          email: z.string().nullable(),
+          avatar_url: z.string().url(),
+        })
+        .parse(githubUserData)
+
+      if (email === null) {
+        throw new BadRequestError(
+          'Your github account must have an email to authenticate',
+        )
+      }
+
+      let user = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!user) {
+        user = await prisma.user.create({
+          data: {
+            email,
+            name,
+            avatarUrl,
+          },
+        })
+      }
+
+      const account = await prisma.account.findUnique({
+        where: {
+          provider_userId: {
+            provider: 'GITHUB',
+            userId: user.id,
+          },
+        },
+      })
+
+      if (!account) {
+        await prisma.account.create({
+          data: {
+            provider: 'GITHUB',
+            providerAccountId: githubId,
+            userId: user.id,
+          },
+        })
+
+        const token = await reply.jwtSign(
+          {
+            sub: user.id,
+          },
+          {
+            sign: {
+              expiresIn: '7d',
+            },
+          },
+        )
+
+        return reply.status(201).send({
+          token,
+        })
+      }
+    },
+  )
+}
