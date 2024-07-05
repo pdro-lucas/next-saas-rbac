47f3a0eb10f629040ef3a2659b9367d97b364919
pdro-lucas <pdrolucas.dev@gmail.com>
2024-07-04 21:48:39 -0300

    feat: Add updateOrganization route to update an organization
    This commit adds a new route `updateOrganization` to update an organization in the API. It includes the necessary validation for the request body and parameters, as well as authorization checks to ensure that only authorized users can update an organization.

---
+++ apps/api/src/http/routes/orgs/update-organization.ts
@@ -0,0 +1,82 @@
+import { organizationSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+import { UnautherizedError } from '../_errors/unauthorized-error'
+
+export async function updateOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations/:slug',
+      {
+        schema: {
+          tags: ['Organizations'],
+          summary: 'Update an organization',
+          security: [{ bearerAuth: [] }],
+          body: z.object({
+            name: z.string(),
+            domain: z.string().nullish(),
+            shouldAttachUsersByDomain: z.boolean().optional(),
+          }),
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            201: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { name, domain, shouldAttachUsersByDomain } = request.body
+
+        const authOrganization = organizationSchema.parse(organization)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('update', authOrganization)) {
+          throw new UnautherizedError(
+            'You are not allowed to update this organization',
+          )
+        }
+
+        if (domain) {
+          const organizationByDomain = await prisma.organization.findFirst({
+            where: { domain, id: { not: organization.id } },
+          })
+
+          if (organizationByDomain) {
+            throw new BadRequestError(
+              'Organization with this domain already exists',
+            )
+          }
+        }
+
+        await prisma.organization.update({
+          where: {
+            id: organization.id,
+          },
+          data: {
+            name,
+            domain,
+            shouldAttachUsersByDomain,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
