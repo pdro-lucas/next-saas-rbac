588bab7e4525ee1de7efae5d367d641ae525f4bf
pdro-lucas <pdrolucas.dev@gmail.com>
2024-07-06 18:18:11 -0300

    feat: Add routes to get project and update project

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -27,6 +27,9 @@ import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { createProject } from './routes/projects/create-project'
 import { deleteProject } from './routes/projects/delete-project'
+import { getProject } from './routes/projects/get-project'
+import { getProjects } from './routes/projects/get-projects'
+import { updateProject } from './routes/projects/update-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -81,5 +84,8 @@ app.register(shutdownOrganization)
 app.register(transferOrganization)
 app.register(createProject)
 app.register(deleteProject)
+app.register(getProject)
+app.register(getProjects)
+app.register(updateProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => console.log('Server running'))
