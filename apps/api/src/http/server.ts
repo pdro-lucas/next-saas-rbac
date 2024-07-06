647cdedd4552bc6231c72e83b0092086a38b0f86
pdro-lucas <pdrolucas.dev@gmail.com>
2024-07-06 20:00:05 -0300

    feat: Add route to get invite

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,6 +19,7 @@ import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { createInvite } from './routes/invites/create-invite'
+import { getInvite } from './routes/invites/get-invite'
 import { getMembers } from './routes/members/get-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMembers } from './routes/members/update-member'
@@ -95,5 +96,6 @@ app.register(getMembers)
 app.register(updateMembers)
 app.register(removeMember)
 app.register(createInvite)
+app.register(getInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => console.log('Server running'))
